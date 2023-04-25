59db46861de3b7f1d32d1ab335ca4e6eede4bfc2
Alexis Kuszaj <114528293+AlexisKuszaj@users.noreply.github.com>
2023-04-25 10:36:02 -0500

    no changes

--- Authors/client/src/components/Create.js
+++ Authors/client/src/components/Create.js
@@ -11,49 +11,51 @@ const Create = (props) => {
     const navigate = useNavigate()
 
     const changeHandler = (e) => {
-        setWriter({ ...writer, [e.target.name]: e.target.value });
-    }
+        setWriter({ ...writer, [e.target.author]: e.target.value });
 
-    const submitHandler = (e) => {
-        e.preventDefault();
 
-        const author = writer.author;
-        let errors = {}
-        if (author.length < 3) {
-            errors.author = { message: 'must be at least 3 characters' }
+        const submitHandler = (e) => {
+            e.preventDefault();
+
+            const { author } = writer;
+            let errors = {}
+            if (!author) {
+                errors.author = { message: 'Author name required' };
+              } else if (author.length < 3) {
+                errors.author = { message: 'Author name must be at least 3 characters' };
+              }
+            if (Object.keys(errors).length > 0) {
+                setErrors(errors);
+            } else {
+                axios
+                    .post('http://localhost:8000/api/newAuthor', { author })
+                    .then((res) => {
+                        console.log(res);
+                        console.log(res.data.writer);
+                        navigate(`/author/${res.data.writer._id}`, { state: { writer: res.data.writer } })
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            }
         }
-        if (Object.keys(errors).length > 0) {
-            setErrors(errors);
-        } else {
-            axios
-                .post('http://localhost:8000/api/newAuthor', { author })
-                .then((res) => {
-                    console.log(res);
-                    console.log(res.data.writer);
-                    navigate(`/author/${res.data.writer._id})`, { state: { writer: res.data.writer } })
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-        }
-    }
 
-    return (
-        <div>
+        return (
             <div>
-                <h1>Favorite Authors</h1>
-                <p>Add your favorite author below: </p>
-                <form onSubmit={submitHandler}>
-                    <div className='fields'>
-                        <label>Author</label>
-                        <input onChange={changeHandler} value={writer.author} name='author' type='text' />
-                        {errors.author && <p>{errors.author.message}</p>}
-                    </div>
-                    <button type='submit'>Submit</button>
-                </form>
+                <div>
+                    <h1>Favorite Authors</h1>
+                    <p>Add your favorite author below: </p>
+                    <form onSubmit={submitHandler}>
+                        <div className='fields'>
+                            <label>Author</label>
+                            <input value={writer.author} name='author' type='text' />
+                            {errors.author && <p>{errors.author.message}</p>}
+                        </div>
+                        <button type='submit' value='submit'>Submit</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 export default Create 
